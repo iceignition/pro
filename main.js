9bbac3cdeeb2f7428be53cea97708d238bae9302
Jaemin An <jaemin9945@mail.hongik.ac.kr>
2026-01-22 22:43:53 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,40 +7,40 @@ window.addEventListener('scroll', () => {
   const pageHeight = document.body.scrollHeight - window.innerHeight;
   const scrollPercent = scrollY / pageHeight;
 
-  if (scrollPercent < 0.5 / 6) {
+  if (scrollPercent < 0.8 / 6) {
     headline.innerHTML = `
       2025<br>
       Creative Direction for 'DNTP'ISSUE 001<br>
       Visual Direction, AI Image Generation
     `;
-  } else if (scrollPercent < 1.5 / 6) {
+  } else if (scrollPercent < 1.8 / 6) {
     headline.innerHTML = `
       2025<br>
       Creative Direction for 'Accumulated Hits'<br>
       Visual Direction, Exhibition Planning, Digital Content Production,<br>
       Graphic Design, VFX
     `;
-  } else if (scrollPercent < 3 / 6) {
+  } else if (scrollPercent < 3.3 / 6) {
     headline.innerHTML = `
       2025 <br>
       Creative Direction for the Graphic Performance Video, 'Chiaroscuro' <br>
       Filming, Editing, VFX, Graphic Design,<br>
       Book Design, Writing, Installation Design
     `;
-  } else if (scrollPercent < 3.8 / 6) {
+  } else if (scrollPercent < 4.0 / 6) {
     headline.innerHTML = `
       2024<br>
       Brand Visual Direction for 'Kyolameth' <br>
       Creative Direction, Brand Identity Design, 3D Graphic Design,<br>
       AI Image Generation
     `;
-  } else if (scrollPercent < 4.8 / 6) {
+  } else if (scrollPercent < 5.1 / 6) {
     headline.innerHTML = `
       2024<br>
       Creative Direction for 'SSGI' Magazine <br>
       Brand Identity Design, Content Creation, Editorial Design
     `;
-  } else if (scrollPercent < 5.4 / 6) {
+  } else if (scrollPercent < 5.6 / 6) {
     headline.innerHTML = `
       2025 <br>
       Visual Direction for 'Kuai Kuai' <br>
